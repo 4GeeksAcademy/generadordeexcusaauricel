d65a52ce78809652efde6283767e687a6c61b86d
Auricel <148658628+Auricel@users.noreply.github.com>
2024-06-14 11:38:26 +0000

    Lologre

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Mi Perro", "Mi Abuela", "Mis tortugas", "Mis pajaros"];
+  let action = ["ate", "peed", "crushed", "broker"];
+  let what = ["my homework", "they keys", "the car"];
+  let when = [
+    "before the class",
+    "rigth on time",
+    "when i finished",
+    "during my luch",
+    "while i was praying"
+  ];
+
+  const getRandomElement = ListElements => {
+    let element = "";
+    element = ListElements[Math.floor(Math.random() * ListElements.length)];
+    return element;
+  };
+  let excusegeneral = document.querySelector("#excuse");
+  excusegeneral.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
 };
